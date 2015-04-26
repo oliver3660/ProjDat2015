90bb3555a5088e2cdf66350b789045f9cc0d2ea6
Thomas Nyegaard-Signori <thomas.nyegaard.signori@gmail.com>
2015-04-26 15:08:25 +0200

    Ændringer til kodemappen v.3

---
+++ Kode/regbog.php
@@ -0,0 +1,11 @@
+<div id="content">
+	<h1>Registrer bog</h1>
+	<form>
+		Forfatter:
+		<input type="text" name="forfatter">
+		Titel:
+		<input type="text" name="titel">
+		<br>
+		<input class="button" type="submit" value="Registrer">
+	</form>
+</div>
---
+++ Kode/regbruger.php
@@ -0,0 +1,11 @@
+<div id="content">
+	<h1>Registrer bruger</h1>
+	<form>
+		Din email:
+		<input type="text" name="email">
+		Navn:
+		<input type="text" name="navn">
+		<br>
+		<input class="button" type="submit" value="Registrer">
+	</form>
+</div>
